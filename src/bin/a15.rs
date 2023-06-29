2cfe733fb4bd4d7f0e84cc44e9df90cf90f42ac8
Egor Fursov <fursov.egor@gmail.com>
2023-06-29 13:58:57 +0300

    16/19

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -10,5 +10,28 @@
 // * Use an enum for the tickets with data associated with each variant
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
+enum Ticket {
+    Backstage(i32, String),
+    Vip(i32, String),
+    Standard(i32),
+}
 
-fn main() {}
+fn main() {
+    let tick = vec![
+        Ticket::Vip(100, "Ivan".to_owned()),
+        Ticket::Backstage(75, "Leha".to_owned()),
+        Ticket::Standard(50),
+    ];
+
+    for t in &tick {
+        match t {
+            Ticket::Backstage(price, name) => {
+                println!("Backstage. Price: {:?}, Name: {:?}", price, name)
+            }
+            Ticket::Vip(price, name) => {
+                println!("VIP. Price: {:?}, Name: {:?}", price, name)
+            }
+            Ticket::Standard(price) => println!("Standard. Price: {:?}", price),
+        }
+    }
+}
--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -8,7 +8,25 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+struct Assignment {
+    name: String,
+    locker: Option<i32>
+}
 
-fn main() {}
+impl Assignment {
+    fn print_details(&self) {
+        match self.locker {
+           Some(mark) => println!("Name: {:?}, Assignment: {:?}", self.name, mark),
+           None =>  println!("Name: {:?}, Assignment: None", self.name)
+        }
+    }
+}
+
+fn main() {
+    let a = Assignment {
+        name: "Assignment name".to_owned(), 
+        locker: None
+    };
+
+    a.print_details();
+}
