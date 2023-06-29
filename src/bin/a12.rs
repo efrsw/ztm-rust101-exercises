ddb96ca5e9dd2dbc41f9f50ebae5a4d6409c4b80
Egor Fursov <fursov.egor@gmail.com>
2023-06-29 11:53:52 +0300

    12/19

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -10,4 +10,53 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Color {
+    Red, 
+    Green
+}
+
+struct Dimensions {
+    height: f64,
+    width: f64,
+}
+
+struct Box {
+    color: Color,
+    weight: i64,
+    dimensions: Dimensions
+}
+
+impl Box {
+    fn new(color: Color, dimensions: Dimensions, weight: i64) -> Self {
+        return Self {
+            color: color, 
+            weight: weight,
+            dimensions: dimensions
+        }
+    }
+
+    fn print_box(&self) -> () {
+        println!("Weight: {:?}", self.weight);
+        println!("Dimensions: {:?}, {:?}", self.dimensions.height, self.dimensions.width);
+        
+        match self.color {
+            Color::Red => println!("Color is red"),
+            Color::Green => println!("Color is green"),
+
+        };
+    }
+}
+
+impl Dimensions {
+    fn new(height: f64, width: f64) -> Self {
+        Self {
+            height,
+            width
+        }
+    }
+}
+
+fn main() {
+    let b = Box::new(Color::Green, Dimensions::new(50.0, 50.0), 75);
+    b.print_box();
+}
